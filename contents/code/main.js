013da078ea4323dd330909723f70953104f68f7a
fin444 <fin444@users.noreply.github.com>
2023-04-14 15:36:32 -0400

    toggle title hiding on screen edge listener

--- contents/code/main.js
+++ contents/code/main.js
@@ -42,7 +42,40 @@ workspace.clientMaximizeSet.connect((client, horizontalMaximized, verticalMaximi
 	}
 });
 
+// screen edge listener
+function screenEdgeActivated() {
+    for (client of workspace.clientList()) {
+    	if (client.active) {
+    		if (isManaged(client)) {
+    			client.noBorder = !client.noBorder;
+    		}
+    		return;
+    	}
+    }
+}
+
+// magic code to register a screen edge listener that the user can then configure in screen edges settings
+// it's just done this way, there isn't really an explanation in the docs
+var registeredBorders = [];
+function initScreenEdges() {
+    for (var i in registeredBorders) {
+        unregisterScreenEdge(registeredBorders[i]);
+    }
+    registeredBorders = [];
+    var borders = readConfig("BorderActivate", "").toString().split(",");
+    for (var i in borders) {
+        var border = parseInt(borders[i]);
+        if (isFinite(border)) {
+            registeredBorders.push(border);
+            registerScreenEdge(border, screenEdgeActivated);
+        }
+    }
+}
+
 // init
 for (client of workspace.clientList()) {
 	clientAdded(client);
 }
+
+options.configChanged.connect(initScreenEdges);
+initScreenEdges();
