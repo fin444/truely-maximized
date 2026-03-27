1d407e4c146380eb8c5ae93fab860cb5e0b2e203
Leopold Luley <git@leopoldluley.de>
2026-03-27 12:48:59 +0100

    make auto hide offset configurable

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,5 +1,6 @@
 // utils
 var shouldHideTiled = false;
+var autoHideAt = 200;
 
 function shouldHideTitle(window) {
 	if (monitorBlacklist.includes(window.output.name)) {
@@ -55,6 +56,7 @@ workspace.windowRemoved.connect((window) => {
 });
 
 function cursorPosChanged() {
+	if (autoHideAt == 0) return;
 	var activeScreen = workspace.activeScreen;
 	if (activeScreen == null) return;
 	var screenHeight = activeScreen.geometry.height;
@@ -63,7 +65,7 @@ function cursorPosChanged() {
 		registeredBorders.length > 0
 		&& isManaged(workspace.activeWindow)
 		&& shouldHideTitle(workspace.activeWindow)
-		&& localCursorPos.y > 150
+		&& localCursorPos.y > autoHideAt
 	) {
 		workspace.activeWindow.noBorder = true;
 	}
@@ -79,7 +81,7 @@ function screenEdgeActivated() {
 	for (window of workspace.windowList()) {
 		if (window.active) {
 			if (isManaged(window) && shouldHideTitle(window)) {
-				window.noBorder = false;
+				window.noBorder = autoHideAt == 0 && !window.noBorder;
 				lastScreenEdge = Date.now();
 			}
 			return;
@@ -110,6 +112,7 @@ function init() {
 	windowBlacklist = readConfig("windowBlacklist", "yakuake").split(",").filter((name) => name.length != 0);
 	monitorBlacklist = readConfig("monitorBlacklist", "").split(",").filter((name) => name.length != 0);
 	shouldHideTiled = readConfig("shouldHideTiled", false);
+	autoHideAt = readConfig("autoHideAt", 200);
 	initScreenEdges();
 }
 options.configChanged.connect(init);
